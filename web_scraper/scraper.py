b650f91000b79420bf92dde21c36f8ab7f0a98f9
Eden Brekke <eden.lorrai@gmail.com>
2022-05-10 16:19:38 -0700

    added page request pull and soup html parser

--- web_scraper/scraper.py
+++ web_scraper/scraper.py
@@ -3,5 +3,9 @@ from bs4 import BeautifulSoup
 
 url = 'https://en.wikipedia.org/wiki/Chernobyl_disaster'
 
-def get_citation_count(url):
-  pass
+def get_citations_needed_count(url):
+  """
+    A function that will take a URL and scrape through the page to find any "citation needed" clauses within
+  """
+  page = requests.get(url)
+  soup = BeautifulSoup(page.content, 'html.parser')
