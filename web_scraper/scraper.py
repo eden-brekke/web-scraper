b2b3e8a3e848e88fd57c3712387ec176c316aca3
Eden Brekke <eden.lorrai@gmail.com>
2022-05-10 19:07:47 -0700

    small changes

--- web_scraper/scraper.py
+++ web_scraper/scraper.py
@@ -30,14 +30,11 @@ def get_citations_needed_report(url):
   # so many parents!
   for result in results:
     text_cites_needed_for.append(result.parent.parent.parent.text)
+
     
   print("The follow text needs citation verification")
-  items = []
   for item in text_cites_needed_for:
-    items.append(item)
     print(item)
-    
-  return items
 
   
   
