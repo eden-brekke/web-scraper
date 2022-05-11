a913380bedef1c67e7c18aeef27b2cbeeb3fac6a
Eden Brekke <eden.lorrai@gmail.com>
2022-05-10 17:07:19 -0700

    got citation needed report functional, parents on parents on parents

--- web_scraper/scraper.py
+++ web_scraper/scraper.py
@@ -1,6 +1,6 @@
-from functools import total_ordering
 import requests
 from bs4 import BeautifulSoup
+
 
 url = 'https://en.wikipedia.org/wiki/Chernobyl_disaster'
 
@@ -18,7 +18,22 @@ def get_citations_needed_count(url):
     
   print(f"The Wikipedia page at url: {url} needs {counter} citations!")
   
-
+def get_citations_needed_report(url):
+  """ 
+  Function that will report the text in which the citation is needed for! 
+  """
+  page = requests.get(url)
+  soup = BeautifulSoup(page.content, 'html.parser')
+  results = soup.find_all(title="Wikipedia:Citation needed")
+  text_cites_needed_for = []
+  # so many parents!
+  for result in results:
+    text_cites_needed_for.append(result.parent.parent.parent.text)
+    
+  print("The follow text needs citation verification")
+  for item in text_cites_needed_for:
+    print(item)
   
   
 get_citations_needed_count(url)
+get_citations_needed_report(url)
